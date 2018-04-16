d4c1ace680e5b2c94a7ab5612907e2036ffe88de
Andrew Usher <andrewusher00@gmail.com>
2018-04-16 10:11:44 -0500

    Add mongodb config

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://andrew:andrew@ds031607.mlab.com:31607/devchat'
+}
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
 const app = express()
+const db = require('./config/keys').mongoURI
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(error => console.log(error))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
