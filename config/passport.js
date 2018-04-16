f552cc97b2357793cb785f75abd6eb594cbd6a38
Andrew Usher <andrewusher00@gmail.com>
2018-04-16 13:55:01 -0500

    Add passport and body parser

---
+++ config/passport.js
@@ -0,0 +1,22 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const mongoose = require('mongoose')
+const User = mongoose.model('users')
+const secret = require('./keys').jwtSecret
+
+const options = {}
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+options.secretOrKey = secret
+
+module.exports = passport => {
+  passport.use(new JwtStrategy(options, (payload, done) => {
+    User.findById(payload.id)
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        } else {
+          return done(null, false)
+        }
+      }).catch(error => console.log(error))
+  }))
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
@@ -7,15 +9,16 @@ const port = process.env.PORT || 3000
 const app = express()
 const db = require('./config/keys').mongoURI
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch(error => console.log(error))
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use(passport.initialize())
 
+require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
