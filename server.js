2e7d2320d8d346e85071eb2772a6fbec2b1d219d
Andrew Usher <andrewusher00@gmail.com>
2018-04-16 10:21:26 -0500

    Add routes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 const port = process.env.PORT || 3000
 const app = express()
 const db = require('./config/keys').mongoURI
@@ -12,6 +15,10 @@ mongoose
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
