dd241ddaf4e1fb0425cc23a7be241e246428dd2f
Andrew Usher <andrewusher00@gmail.com>
2018-04-17 12:50:57 -0500

    Start profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,80 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 const router = express.Router()
 
 // @route GET api/posts/hello
 // @desc Tests profile route
 // @access Public
 router.get('/hello', (req, res) => res.json({ message: 'Profile works' }))
+
+// @route GET api/profile
+// @desc Get current user's profile
+// @access Private
+router.get('/', passport.authenticate('jwt', { session: false }, (req, res) => {
+  const errors = {}
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = 'There is no profile for this user'
+        return res.status(404).json(errors)
+      }
+      // Return profile
+      res.json(profile)
+    })
+    .catch(error => res.status(404).json(error))
+}))
+
+// @route POST api/profile
+// @desc Create user profile
+// @access Private
+router.get('/', passport.authenticate('jwt', { session: false }, (req, res) => {
+  const errors = {}
+  const { id } = req.user
+  // Get fields
+  let profileFields = {}
+  profileFields.user = id
+  if (req.body.handle) profileFields.handle = req.body.handle
+  if (req.body.company) profileFields.company = req.body.company
+  if (req.body.website) profileFields.website = req.body.website
+  if (req.body.location) profileFields.location = req.body.location
+  if (req.body.bio) profileFields.bio = req.body.bio
+  if (req.body.status) profileFields.status = req.body.status
+  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername
+
+  if (typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',')
+  }
+
+  profileFields.social = {}
+  if (req.body.youtube) profileFields.social.youtube = req.body.youtube
+  if (req.body.twitter) profileFields.social.twitter = req.body.twitter
+  if (req.body.facebook) profileFields.social.facebook = req.body.facebook
+  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+  if (req.body.instagram) profileFields.social.instagram = req.body.instagram
+
+  Profile.findOne({ user: id }).then(profile => {
+    if (profile) {
+      Profile.findOneAndUpdate({ user: id }, { $set: profileFields }, { new: true })
+        .then(profile => res.json(profile))
+        .catch(error => res.status(400).json(error))
+    } else {
+      Profile.findOne({ handle: profileFields.handle })
+        .then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists'
+            res.status(400).json(errors)
+          }
+
+          new Profile(profileFields)
+            .save()
+            .then(profile => res.json(profile))
+        })
+    }
+  })
+}))
 
 module.exports = router
