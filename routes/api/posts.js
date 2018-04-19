87579450857bab5180bd811f0916f99822a2b4ee
Andrew Usher <andrewusher00@gmail.com>
2018-04-19 10:15:20 -0500

    Add validation to post create route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
+
+// Model
 const Post = require('../../models/Post')
+
+// Validation for post
+const validatePostInput = require('../../validation/post')
 
 // @route GET api/posts/hello
 // @desc Tests post route
@@ -14,6 +19,12 @@ router.get('/hello', (req, res) => res.json({ message: 'Posts works' }))
 // @access Private
 
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   const { name, text, id } = req.user
   const newPost = new Post({
     text,
