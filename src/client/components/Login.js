f1fa22c276d62d06d753b28ba1689038aef4ca76
Andrew Usher <andrewusher00@gmail.com>
2018-04-23 08:01:27 -0500

    Complete login form

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    this.formChange = this.formChange.bind(this)
+    this.formSubmit = this.formSubmit.bind(this)
+  }
+
+  formChange (event) {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  formSubmit (event) {
+    event.preventDefault()
+
+    const { email, password } = this.state
+    const user = {
+      email,
+      password
+    }
+
+    console.log(user)
+  }
   render () {
     return (
-      <div>Login page</div>
+      <div className="login">
+        <div className="container">
+          <p>Sign in to Devchat</p>
+          <form onSubmit={this.formSubmit} >
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" onChange={this.formChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" onChange={this.formChange} />
+            </div>
+            <input type="submit" value="Login" />
+          </form>
+        </div>
+      </div>
     )
   }
 }
