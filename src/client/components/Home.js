f09c99a876f1f15d233cdd5fb03b6580d537359e
Andrew Usher <andrewusher00@gmail.com>
2018-04-26 21:22:41 -0500

    Change layout of home page

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/_home.styl'
 
 const Home = () => (
   <main className="landing">
     <div className="landing-text">
-      <h2>Welcome to Devchat!</h2>
-      <h3>Create a profile with your skills, and chat with other developers</h3>
+      <h4>Welcome to Devchat</h4>
+      <h2>A Friendly and Inclusive Dev Community</h2>
+      <h3>Create a profile to show off your skills and chat with other developers</h3>
       <div className="landing-buttons">
-        <Link to="/register" />
-        <Link to="/login" />
+        <Link to="/register">Register</Link>
       </div>
     </div>
   </main>
