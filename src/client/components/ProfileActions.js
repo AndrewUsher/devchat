02de7f2fa4a8bb86059f6a7e1ea4654dbdd676d4
Andrew Usher <andrewusher00@gmail.com>
2018-04-28 00:51:03 -0500

    Complete profile actions component

---
+++ src/client/components/ProfileActions.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/_profile-actions.styl'
+
+const ProfileActions = () => (
+  <div className="profile-actions">
+    <Link to="/edit-profile" >
+      Edit Profile
+    </Link>
+    <Link to="/add-experience">
+      Add Experience
+    </Link>
+    <Link to="/add-education" >
+      <i />
+      Add Education
+    </Link>
+  </div>
+
+)
+
+export default ProfileActions
