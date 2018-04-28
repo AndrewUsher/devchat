f0dcc708425d7b18969b8fdf2226cd27e05af21b
Andrew Usher <andrewusher00@gmail.com>
2018-04-28 00:56:03 -0500

    Add profile education component

---
+++ src/client/components/Education.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Moment from 'react-moment'
+import { deleteEducation } from '../actions/profile'
+
+class Education extends Component {
+  onDeleteClick (id) {
+    this.props.deleteEducation(id)
+  }
+
+  render () {
+    const education = this.props.education.map(edu => (
+      <tr key={edu._id}>
+        <td>{edu.school}</td>
+        <td>{edu.degree}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {edu.to === null ? (
+            ' Now'
+          ) : (
+            <Moment format="YYYY/MM/DD">{edu.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            onClick={this.onDeleteClick.bind(this, edu._id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ))
+    return (
+      <div>
+        <h4>Education Credentials</h4>
+        <table>
+          <thead>
+            <tr>
+              <th>School</th>
+              <th>Degree</th>
+              <th>Time Period</th>
+              <th>Remove</th>
+            </tr>
+            {education}
+          </thead>
+        </table>
+      </div>
+    )
+  }
+}
+
+Education.propTypes = {
+  deleteEducation: PropTypes.func.isRequired,
+  education: PropTypes.shape({
+    map: PropTypes.func
+  })
+}
+
+export default connect(null, { deleteEducation })(Education)
