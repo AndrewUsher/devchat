cf4f27e514c65d8863f85f421a8f7e06ce6d8615
Andrew Usher <andrewusher00@gmail.com>
2018-04-28 19:30:41 -0500

    Remove delete row in experience

--- src/client/components/Experience.js
+++ src/client/components/Experience.js
@@ -2,32 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
-import { deleteExperience } from '../actions/profile'
 
 class Experience extends Component {
-  onDeleteClick (id) {
-    this.props.deleteExperience(id)
-  }
-
   render () {
     const experience = this.props.experience.map(exp => (
       <tr key={exp._id}>
         <td>{exp.company}</td>
         <td>{exp.title}</td>
         <td>
-          <Moment format="YYYY/MM/DD">{exp.from}</Moment>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
           {exp.to === null ? (
             ' Now'
           ) : (
             <Moment format="YYYY/MM/DD">{exp.to}</Moment>
           )}
         </td>
-        <td>
-          <button onClick={this.onDeleteClick.bind(this, exp._id)}>
-            Delete
-          </button>
-        </td>
-      </tr>
+      </tr >
     ))
     return (
       <div className="experience">
@@ -39,7 +29,6 @@ class Experience extends Component {
                 <th>Company</th>
                 <th>Title</th>
                 <th>Years</th>
-                <th>Remove</th>
               </tr>
               {experience}
             </thead>
@@ -51,10 +40,7 @@ class Experience extends Component {
 }
 
 Experience.propTypes = {
-  deleteExperience: PropTypes.func.isRequired,
-  experience: PropTypes.shape({
-    map: PropTypes.func
-  })
+  experience: PropTypes.array
 }
 
-export default connect(null, { deleteExperience })(Experience)
+export default connect(null)(Experience)
