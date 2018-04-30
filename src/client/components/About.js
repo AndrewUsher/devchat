b48bd59be07d21a51c0e13ea0cd8c35f1f4c2744
Andrew Usher <andrewusher00@gmail.com>
2018-04-29 20:26:16 -0500

    Add About component

---
+++ src/client/components/About.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const About = (props) => {
+  const { profile } = props
+
+  const firstName = profile.user.name.trim().split(' ')[0]
+
+  const skills = profile.skills.map((skill, index) => (
+    <div key={index}>
+      {skill}
+    </div>
+  ))
+
+  return (
+    <div className="profile-about">
+      <h3>{firstName}'s Bio</h3>
+      <p>
+        {profile.bio ? (
+          <span>{firstName} does not have a bio</span>
+        ) : (
+          <span>{profile.bio}</span>
+        )}
+      </p>
+      <h3>Skill Set</h3>
+      <div>
+        {skills}
+      </div>
+    </div>
+  )
+}
+
+About.propTypes = {
+  profile: PropTypes.object.isRequired
+}
+
+export default About
