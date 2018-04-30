4bb61c24688287a537e4efb41efe89d38f73eb07
Andrew Usher <andrewusher00@gmail.com>
2018-04-29 20:07:57 -0500

    Add ProfileItem component

---
+++ src/client/components/ProfileItem.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import '../styles/_profile-item.styl'
+
+const ProfileItem = ({ profile }) => (
+  <div className="profile-item">
+    <img src={profile.user.avatar} alt={profile.user.name} />
+    <div>
+      <h3>{profile.user.name}</h3>
+      <h4>
+        {profile.company && (
+          <span>at {profile.company}</span>
+        )}
+      </h4>
+      <h4>
+        {profile.location && (
+          <span>{profile.location}</span>
+        )}
+      </h4>
+      <Link to={`/profile/${profile.handle}`} className="view-profile">
+        View Profile
+      </Link>
+      <h5>Skill Set</h5>
+      <ul>
+        {profile.skills.slice(0, 4).map((skill, index) => (
+          <li key={index} >
+            <i />
+            {skill}
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+)
+
+ProfileItem.propTypes = {
+  profile: PropTypes.object.isRequired
+}
+
+export default ProfileItem
