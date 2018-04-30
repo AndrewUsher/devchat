c03f6a4d1f7b717a09ffbd57f93f20fc43abf049
Andrew Usher <andrewusher00@gmail.com>
2018-04-29 19:20:51 -0500

    Add styles to loader

--- src/client/components/Loader.js
+++ src/client/components/Loader.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { BounceLoader } from 'react-spinners'
 
+const styles = {
+  display: 'block',
+  margin: 'auto'
+}
 const Loader = () => (
-  <BounceLoader size={100} />
+  <BounceLoader size={100} style={styles} />
 )
 
 export default Loader
