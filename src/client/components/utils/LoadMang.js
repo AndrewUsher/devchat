6477442aaadeb39a7fc271631a68c08bd22422b1
Andrew Usher <andrewusher00@gmail.com>
2018-05-03 20:27:19 -0500

    Add react-loadable util component

---
+++ src/client/components/utils/LoadMang.js
@@ -0,0 +1,11 @@
+
+import Loadable from 'react-loadable'
+import Loader from './Loader'
+
+const Loadmang = func =>
+  Loadable({
+    loading: Loader,
+    loader: func
+  })
+
+export default Loadmang
